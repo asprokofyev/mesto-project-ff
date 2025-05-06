75f62513b7f3706057973c621b3e4115c612b250
Alexey Prokofyev <a.s.prokofyev1976@yandex.ru>
2025-05-07 00:03:06 +0700

    реализовал функцилнальность согласно ТЗ

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+git@github.com:asprokofyev/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardName, cardLink, deleteCard) {
+  // создаем экземпляр карточки клонированем шаблона
+  const cardInstance = cardTemplate
+    .querySelector('.places__item')
+    .cloneNode(true);
+  // заполняем карточку данными
+  cardInstance.querySelector('.card__image').src = cardLink;
+  cardInstance.querySelector('.card__title').textContent = cardName;
+  // вешаем обработчик события на кнопку удаления
+  cardInstance
+    .querySelector('.card__delete-button')
+    .addEventListener('click', deleteCard);
+
+  return cardInstance;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  // событие срабатывает на кнопке
+  // карточка является родителем для кнопки
+  // значит чтобы удалить карточку, надо удалить родителя кнопки
+  evt.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) =>
+  cardsContainer.append(createCard(item.name, item.link, deleteCard))
+);
