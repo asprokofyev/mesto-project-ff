5036b6cd0f15055eb339fdcb65321b0dcd20626e
Alexey Prokofyev <a.s.prokofyev1976@yandex.ru>
2025-06-17 14:55:58 +0700

    делаю проектную работу 7 спринта

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,54 @@
-import { initialCards } from './cards.js';
+import { initialCards } from "./cards.js";
 
-import { createCard, deleteCard, displayCard, likeCard } from './card.js';
+import { createCard, deleteCard, displayCard, likeCard } from "./card.js";
 
-import { closeModal, closeModalByClick, openModal } from './modal.js';
+import { closeModal, closeModalByClick, openModal } from "./modal.js";
+
+// данные для работы с сервером
+const groupId = "wff-cohort-41";
+const myToken = "3f733f72-cb5d-49ce-aae5-bd2f7b148895";
+
+// сообщение об ошибке и регулярное выражение
+const errorWrongSymbols =
+  "Поле может содержать только латинские и кириллические буквы, знаки дефиса и пробелы.";
+const regexpWrongSymbols = "/[a-zа-я0-9ё\\- ]+/gi";
 
 // Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // DOM узлы
 // контейнер карточек
-const cardsContainer = document.querySelector('.places__list');
+const cardsContainer = document.querySelector(".places__list");
 // контейнер профиля
-const profileContainer = document.querySelector('.profile');
+const profileContainer = document.querySelector(".profile");
 // имя в профиле
-const profileTitle = profileContainer.querySelector('.profile__title');
+const profileTitle = profileContainer.querySelector(".profile__title");
 // описание в профиле
 const profileDescription = profileContainer.querySelector(
-  '.profile__description'
+  ".profile__description"
 );
 // кнопка редактирования профиля
 const profileEditButton = profileContainer.querySelector(
-  '.profile__edit-button'
+  ".profile__edit-button"
 );
 // кнопка добавления карточки
 const profileAddCardButton = profileContainer.querySelector(
-  '.profile__add-button'
+  ".profile__add-button"
 );
 
 // массив модальных окон
-const modals = document.querySelectorAll('.popup');
+const modals = document.querySelectorAll(".popup");
 // модальное окно с формой добавления новой карточки
-const addCardModal = document.querySelector('.popup_type_new-card');
+const addCardModal = document.querySelector(".popup_type_new-card");
 // модальное окно для просмотра большой картинки
-const viewImageModal = document.querySelector('.popup_type_image');
+const viewImageModal = document.querySelector(".popup_type_image");
 // модальное окно с формой редактирования профиля
-const editProfileModal = document.querySelector('.popup_type_edit');
+const editProfileModal = document.querySelector(".popup_type_edit");
 
 // форма добавления новой карточки
-const addCardForm = document.forms['new-place'];
+const addCardForm = document.forms["new-place"];
 // форма редактирования профиля
-const editProfileForm = document.forms['edit-profile'];
+const editProfileForm = document.forms["edit-profile"];
 // поле Имя
 const profileNameInput = editProfileForm.name;
 // поле Занятие
@@ -47,8 +56,8 @@ const profileDescriptionInput = editProfileForm.description;
 
 // Функция просмотра большой картинки
 function viewImage(evt) {
-  const image = viewImageModal.querySelector('.popup__image');
-  const imageCaption = viewImageModal.querySelector('.popup__caption');
+  const image = viewImageModal.querySelector(".popup__image");
+  const imageCaption = viewImageModal.querySelector(".popup__caption");
 
   // заполняем модальное окно данными картинки
   image.src = evt.target.src;
@@ -67,7 +76,7 @@ function openAddCardForm() {
 
 // Функция обработки отправки формы добавления карточки
 function handleAddCardFormSubmit(evt) {
-  const newCardName = addCardForm.elements['place-name'];
+  const newCardName = addCardForm.elements["place-name"];
   const newCardImageURL = addCardForm.link;
 
   // сбрасываем обработку события по-умолчанию
@@ -84,7 +93,7 @@ function handleAddCardFormSubmit(evt) {
       likeCard,
       viewImage
     ),
-    'start'
+    "start"
   );
 
   // закрываем модальное окно
@@ -134,18 +143,18 @@ initialCards.forEach((item) =>
 );
 
 // Прикрепляем обработчик к кнопке редактирования профиля
-profileEditButton.addEventListener('click', openEditProfileForm);
+profileEditButton.addEventListener("click", openEditProfileForm);
 
 // Прикрепляем обработчик к кнопке добавления карточки
-profileAddCardButton.addEventListener('click', openAddCardForm);
+profileAddCardButton.addEventListener("click", openAddCardForm);
 
 //Прикрепляем обработчик клика на кнопку закрытия попапа или на оверлей
 modals.forEach((modal) => {
-  modal.addEventListener('click', closeModalByClick);
+  modal.addEventListener("click", closeModalByClick);
 });
 
 // Прикрепляем обработчик отправки формы редактирвоания профиля
-addCardForm.addEventListener('submit', handleAddCardFormSubmit);
+addCardForm.addEventListener("submit", handleAddCardFormSubmit);
 
 // Прикрепляем обработчик отправки формы добавления новой карточки
-editProfileForm.addEventListener('submit', handleEditProfileFormSubmit);
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
